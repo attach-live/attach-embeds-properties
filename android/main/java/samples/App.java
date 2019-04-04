5b647f4c362df59a6b12b6debe565ddf02be4104
Oliver Hausler <oliver@closeup.cc>
2019-04-04 12:36:15 -0700

    Revert "Order properties"
    This reverts commit e3f176fc0e8d67f131ed4d796fa6e78364b59e2e.

--- android/main/java/samples/App.java
+++ android/main/java/samples/App.java
@@ -17,5 +17,9 @@ public class App extends Application {
         // Help us by sending anonymous error reports (default: true).
         // https://documentation.attach.live/android/concepts/error-reporting
         AttachSdk.setErrorReporting(true);
+
+        // Enable logging to Android Studio (default: false).
+        // You may want to disable in production
+        AttachSdk.setLoggingEnabled(false);
     }
 }
